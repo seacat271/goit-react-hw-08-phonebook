fbf5621b2065068e2343338bc2fea1d029c0e9ad
seacat271 <seacat271083@gmail.com>
2022-10-09 20:13:02 +0300

    add persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactSlice } from './contacts/contactsSlice';
 import { filterSlice } from './filter/filterSlice';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist';
 
 const preLoadedState = {
   contacts: {
@@ -11,11 +23,27 @@ const preLoadedState = {
   filter: '',
 }
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  // blacklist: ['filter'],
+}
+const rootReducer = combineReducers({contacts: contactSlice.reducer, filter: filterSlice.reducer,})
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const middleware = getDefaultMiddleware =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  });
+
 export const store = configureStore({
-  reducer: {
-    contacts: contactSlice.reducer,
-    filter: filterSlice.reducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  reducer: persistedReducer,
+  middleware,
   preLoadedState,
 });
+
+
+
+export const persistor = persistStore(store);
+
