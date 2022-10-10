d9515b4b8161b533ea6b8c3df6435242f675f3af
seacat271 <seacat271083@gmail.com>
2022-10-10 12:14:43 +0300

    Update api

--- src/utils/mockApi.js
+++ src/utils/mockApi.js
@@ -4,3 +4,4 @@ axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 export const addContactFetch = (value) => axios.post('/contacts', value);
 export const deleteContactFetch = (value) => axios.delete(`/contacts/${value}`)
 export const allContactFetch = () => axios.get('/contacts');
+export const updateContactFetch = (value) => axios.patch(`/contacts/${value.id}`, value)
