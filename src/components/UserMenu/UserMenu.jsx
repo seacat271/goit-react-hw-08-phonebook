c496bdfc6265cc44e83d35778ca0a61ba8df78aa
seacat271 <seacat271083@gmail.com>
2022-10-10 11:35:24 +0300

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export const UserMenu = () => {
     return (
      <div>
-        <h1>Helo friend</h1>
+        <h1>Hello friend</h1>
        <button type="button">Exit</button>
      </div>
 
