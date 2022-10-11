37804e5b0f6360fdc00d1248746d736122e3d502
seacat271 <105187184+seacat271@users.noreply.github.com>
2022-10-11 08:58:57 +0000

    Deploying to gh-pages from @ seacat271/goit-react-hw-08-phonebook@672948952bb05468cead486f66107375d92f6ce4 🚀

---
+++ static/js/708.6f893724.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[708],{1708:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("h1",{children:"Welcome to Private Phonebook Service"})})}}}]);
+//# sourceMappingURL=708.6f893724.chunk.js.map
