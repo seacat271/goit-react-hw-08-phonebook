e9eae4aec890832168d9ae94d63fb4e8bf1c8d2b
seacat271 <seacat271083@gmail.com>
2022-10-13 15:29:35 +0300

    checked slice

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -2,14 +2,15 @@ import { createSlice } from "@reduxjs/toolkit"
 import { register, login, logout, refresh } from "./authOperations"
 
 const initialState ={
+    isLoading: false,
+    error: null,
     user: {
         name: null,
         email: null,
     },
     token: null, 
     isLoggedIn: false,
-    isLoading: false,
-    error: null,
+    
 }
 
 export const authSlice = createSlice({
@@ -17,40 +18,40 @@ export const authSlice = createSlice({
     initialState,
     extraReducers: {
     [register.pending] (state) {
-            return state
+            return {...state, isLoading: !state.isLoading, error: null,}
     },
     [register.fulfilled] (state, action) {
-            return {...state, ...action.payload, isLoggedIn: true}
+            return {...state, ...action.payload, isLoggedIn: !state.isLoggedIn, isLoading: !state.isLoading,}
     },
-    [register.rejected] (state) {
-        return state
+    [register.rejected] (state, action) {
+        return {...state, isLoading: !state.isLoading, error: action.payload}
     },
     [login.pending] (state) {
-        return state
+        return {...state, isLoading: !state.isLoading, error: null,}
     },
     [login.fulfilled] (state, action) {
-        return {...state, ...action.payload, isLoggedIn: true}
+        return {...state, ...action.payload, isLoggedIn: !state.isLoggedIn, isLoading: !state.isLoading,}
     },
-    [login.rejected] (state) {
-        return state
+    [login.rejected] (state, action) {
+        return {...state, isLoading: !state.isLoading, error: action.payload}
     },
     [logout.pending] (state) {
-        return state
+        return {...state, isLoading: !state.isLoading, error: null,}
     },
     [logout.fulfilled] (state) {
         return {...state, ...initialState}
     },
-    [logout.rejected] (state) {
-        return state
+    [logout.rejected] (state, action) {
+        return {...state, isLoading: !state.isLoading, error: action.payload}
     },
     [refresh.pending] (state) {
-        return {...state}
+        return {...state, isLoading: !state.isLoading, error: null,}
     },
     [refresh.fulfilled] (state, action) {
-        return {...state, user: {...action.payload}, isLoggedIn: true,}
+        return {...state, user: {...action.payload}, isLoggedIn: !state.isLoggedIn, isLoading: !state.isLoading,}
     },
-    [refresh.rejected] (state) {
-        return {...state}
+    [refresh.rejected] (state, action) {
+        return {...state, isLoading: !state.isLoading, error: action.payload}
     },
 
 }})
